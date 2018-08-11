beb656be22d200d9b97b7655d472fb381f8fab64
Omkar <coolomi444@gmail.com>
2018-08-11 20:42:29 +0530

    initial commit

---
+++ exp.js
@@ -0,0 +1,15 @@
+// 1st way to export module
+/*
+const name = "Omkar S";
+
+module.exports.ddd = name;
+*/
+
+// 2nd way 
+
+exports.ddd = {
+    name : 'Omkar Sawant',
+    add: function(){
+        return 5*5
+    }
+};
---
+++ main.js
@@ -0,0 +1,14 @@
+/*for (var i = 1; i <= 10; i++) {
+    alert("Hello");
+    console.log(i);
+}*/
+
+//Global Objects of node JS.
+// __dirname, __filename, require, module, exports, console
+
+console.log(__dirname);
+console.log(__filename);
+
+const nm = require('./exp');
+console.log(nm.ddd.add());
+
