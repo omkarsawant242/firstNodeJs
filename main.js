301e498c8874670d64eabfd9915f208db8b80e35
--global <>
2018-08-19 20:36:33 +0530

    comment added

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-// // for (var i = 1; i <= 10; i++) {
-// //      alert("Hello");
-// //      console.log(i);
-// //  }
+// // // for (var i = 1; i <= 10; i++) {
+// // //      alert("Hello");
+// // //      console.log(i);
+// // //  }
 
-// // Global Objects of node JS.
-// // __dirname, __filename, require, module, exports, console
+// // // Global Objects of node JS.
+// // // __dirname, __filename, require, module, exports, console
 
-// console.log(__dirname);
-// console.log(__filename);
+// // console.log(__dirname);
+// // console.log(__filename);
 
-// const nm = require('./exp');
-// console.log(nm.ddd.add());
+// // const nm = require('./exp');
+// // console.log(nm.ddd.add());
 
 let a;
 let a;
